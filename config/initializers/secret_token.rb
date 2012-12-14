4ca4e5762aeaa683ddc13949b7502473f724ef1d
mostlyclicks <carlos@mostlyclicks.com>
2012-12-13 20:51:20 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tdr::Application.config.secret_token = '521af9b763aac62d6b99a4c1c9748258ea62bc9b632bb07d845331ff7d98f0706d538c77030ac718ca384efee348b18479fd967cfe5a7597989e6262eadf967b'
